9c3588206b4004edb009012b3fd2fe3282b0fec7
sorapatna <sorapatna@gmail.com>
2026-02-13 09:38:23 +0530

    Add files via upload

---
+++ components/Alert-2.tsx
@@ -0,0 +1,22 @@
+import cn from 'classnames'
+
+export default function Alert  ()  {
+
+  return (
+    <div
+      className={cn('mt-10 ml-5 text-base md:text-lg lg:text-xl'
+      )}
+    >
+        <>
+            <strong>NEW EVENT IN MARCH @)@^.</strong>{' '}
+            <a
+                href={`https://wa.me/+918578003221`}
+                target="_blank" rel="noopener noreferrer"
+                className="underline text-red-700 hover:text-success duration-200 transition-colors"
+            >
+                Look Event Calendar below For more details
+            </a>
+        </>
+    </div>
+  )
+}
