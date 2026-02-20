82f6367bd0a957725788b671f4aa1dff1676ebcb
sorapatna <sorapatna@gmail.com>
2026-02-20 14:24:51 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,29 @@
 import Image from 'next/image';
 
-
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-5 md:gap-28 lg:py-10 lg:flex-row mb-40">
-      <div className="flexCenter max-container absolute object-cover" >
+    <section className="relative w-screen h-screen flex flex-col gap-20 py-5 md:gap-28 lg:py-10 lg:flex-row mb-40">
+      {/* Background Image */}
+      <div className="absolute inset-0">
         <Image
-          src='/images/EVENT.webp'
-          alt='yoga'  
-          width={1440}
-          height={580}
+          src="/images/EVENT.webp"
+          alt="yoga"
+          fill
           priority
-          className="w-screen h-screen object-cover object-center 2xl:rounded-s-xl"
+          className="object-cover object-center"
         />
       </div>
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 ml-5 "> 
-      
+
+      {/* Left Content */}
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 ml-5">
+        {/* Add your text/content here */}
       </div>
 
-      <div className="relative  flex flex-1 items-start mb-60">
-        <div className="relative z-20 flex w-[320px] flex-col gap-4 rounded-3xl bg-yellow-400 px-4 py-4">
+      {/* Right Content */}
+      <div className="relative z-20 flex flex-1 items-start mb-60">
+        <div className="flex w-[320px] flex-col gap-4 rounded-3xl bg-yellow-400 px-4 py-4">
           <div className="flex flex-col">
-            <div className="flexBetween">
+            <div className="flex justify-between">
               <p className="regular-16 text-black">Our Call Sign & Grid-Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
@@ -35,27 +37,10 @@ const Hero = () => {
             <p className="regular-16 block text-black">E-mail:</p>
             <p className="bold-16 text-black">sorapatna@gmail.com</p>
           </div>
-
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Hero
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Hero;
