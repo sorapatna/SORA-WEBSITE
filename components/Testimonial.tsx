2746abc3a0acf8ab4e7eb9ed96ec87a9759657a2
sorapatna <sorapatna@gmail.com>
2026-03-04 10:25:02 +0530

    Update Testimonial.tsx

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -44,83 +44,79 @@ const postData: DataType[] = [
 
 
 export default class MultipleItems extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      speed: 500,
+      autoplaySpeed: 5000,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: { slidesToShow: 3, slidesToScroll: 1 },
+        },
+        {
+          breakpoint: 800,
+          settings: { slidesToShow: 2, slidesToScroll: 1 },
+        },
+        {
+          breakpoint: 600,
+          settings: { slidesToShow: 1, slidesToScroll: 1 },
+        },
+      ],
+    };
 
-    render() {
-        const settings = {
-            dots: false,
-            dotsClass: "slick-dots",
-            infinite: true,
-            slidesToShow: 3,
-            // centerMode: true,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            speed: 500,
-            autoplaySpeed: 5000,
-            cssEase: "linear",
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                }
-            ]
-        };
-
-        return (
-            <div className="bg-amber-300 pt-40 pb-10 sm:pb-32 lg:py-32 " id="testimonial">
-                <div className='mx-auto max-w-7xl sm:py-4 lg:px-8'>
-                <h2 className="1h-82 text-6xl text-black md:text-55xl text-center md:text-start font-semibold"> What People<br />Say?</h2>
-                    <Slider {...settings}>
-                        {postData.map((items, i) => (
-                            <div key={i}>
-                                <div className={`bg-white m-4 p-5 my-40 overflow-hidden rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:shadow-2xl  relative ${i % 2 ? 'middleDiv' : 'testimonial-shadow '}`}>
-                                    <h3 className='text-base text-justify font-normal text-black my-4'>{items.comment}</h3>
-                                    <hr style={{ color: "#D7D5D5" }} />
-                                    <div className="flex justify-between">
-                                        <div>
-                                            <h4 className='text-lg font-medium text-shadow-slate-400 pt-4 pb-2'>{items.name}</h4>
-                                            <h4 className='text-sm font-normal text-black pb-2'>{items.profession}</h4>
-                                        </div>
-                                        <div className="flex">
-                                            <StarIcon width={20} className="text-orange-300" />
-                                            <StarIcon width={20} className="text-orange-300" />
-                                            <StarIcon width={20} className="text-orange-300" />
-                                            <StarIcon width={20} className="text-orange-300" />
-                                            <StarIcon width={20} className="text-amber-500" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </Slider>
+    return (
+      <div
+        className="bg-linear-to-r from-green-400 via-blue-500 to-purple-600 pt-40 pb-10 sm:pb-32 lg:py-32"
+        id="testimonial"
+      >
+        <div className="mx-auto max-w-7xl sm:py-4 lg:px-8">
+          <h2 className="leading-20.5 text-6xl text-black md:text-5xl text-center md:text-start font-semibold">
+            What Our Lovely <br /> Client Says ?
+          </h2>
+          <Slider {...settings}>
+            {postData.map((items, i) => (
+              <div key={i}>
+                <div
+                  className={`bg-white m-4 p-5 my-40 overflow-hidden rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:shadow-2xl relative ${
+                    i % 2 ? "middleDiv" : "testimonial-shadow"
+                  }`}
+                >
+                  <h3 className="text-base text-justify font-normal text-black my-4">
+                    {items.comment}
+                  </h3>
+                  <hr className="border-gray-300" />
+                  <div className="flex justify-between">
+                    <div>
+                      <h4 className="text-lg font-medium text-shadow-slate-400 pt-4 pb-2">
+                        {items.name}
+                      </h4>
+                      <h4 className="text-sm font-normal text-black pb-2">
+                        {items.profession}
+                      </h4>
+                    </div>
+                    <div className="flex">
+                      <StarIcon className="w-5 h-5 text-orange-300" />
+                      <StarIcon className="w-5 h-5 text-orange-300" />
+                      <StarIcon className="w-5 h-5 text-orange-300" />
+                      <StarIcon className="w-5 h-5 text-orange-300" />
+                      <StarIcon className="w-5 h-5 text-amber-500" />
+                    </div>
+                  </div>
                 </div>
-            </div>
-
-        );
-    }
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    );
+  }
 }
 
 
