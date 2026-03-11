dac961263566d4952b83bd3b77486ec7846adfb2
sorapatna <sorapatna@gmail.com>
2026-03-12 01:31:01 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       {/* Background Image */}
       <div className="absolute inset-0 -z-10">
         <Image
-          src="/images/SORA.png"
+          src="/images/SORA-1.png"
           alt="yoga"
           fill
           priority
@@ -45,6 +45,7 @@ const Hero = () => {
 };
 
 export default Hero;
+
 
 
 
