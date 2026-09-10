d706d1cb731a6fe8c380e461869ef57ce25050e3
sorapatna <sorapatna@gmail.com>
2026-09-10 19:17:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import Newsletter from "@/components/Subscribe";
 import Latest from "@/components/LatestPost";
 import dynamic from "next/dynamic";
 import { Metadata } from 'next';
-import Alert-2 from "@/components/Alert-2";
 
 
 const YouTubeFrame = dynamic(() => import("@/components/Video"));
@@ -22,7 +21,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-     <Alert-2 />
       <Hero />
       <Guide />
       <Features />
