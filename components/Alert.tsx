0d071537b78b04ac60fb49ea3a9d3d05e5031c43
sorapatna <sorapatna@gmail.com>
2026-09-10 19:19:43 +0530

    Update Alert.tsx

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -10,7 +10,7 @@ export default function Alert  ()  {
         <>
             <strong>We are available on 145.100 MHZ(Simplex) as well as on Talk Group-27304 (Digital).</strong>{' '}
             <a
-                href={`https://wa.me/+918578003221`}
+                href={`https://hifi2026.nitk.ac.in`}
                 target="_blank" rel="noopener noreferrer"
                 className="underline text-red-700 hover:text-success duration-200 transition-colors"
             >
